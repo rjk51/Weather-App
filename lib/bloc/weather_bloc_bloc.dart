16f69090ebf3a3fcf3af1077e745b5f0673c52d0
rjk51 <ayaanrjk2004@gmail.com>
2024-03-26 00:12:48 +0530

    initial commit

---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+import 'package:weather_app/data/my_data.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  WeatherBloc() : super(WeatherInitial()) {
+    on<FetchWeather>((event, emit) async {
+      emit(WeatherLoading());
+      try {
+        WeatherFactory wf = WeatherFactory(API_KEY,
+            language: Language.ENGLISH);
+            
+        Weather weather = await wf.currentWeatherByLocation(
+          event.position.latitude,
+          event.position.longitude,
+        );
+        emit(WeatherSuccess(weather));
+      } catch (e) {
+        emit(WeatherFailure());
+      }
+    });
+  }
+}
